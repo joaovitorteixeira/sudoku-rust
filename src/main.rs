a88afba1d5863084bde781915865c06ae8f9f1e6
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2025-12-12 08:22:58 +1000

    fix: build board correctly

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
+use std::fs::read_to_string;
+
 pub mod sudoku;
 
 fn main() {
-    let mut columns: Vec<&[Option<u8>]> = Vec::new();
-    let mut index = 0;
+    let board_file_result = read_file("input.txt".to_owned());
+    let board_file = match board_file_result {
+        Ok(board_file) => board_file,
+        Err(msg) => panic!("{}", msg),
+    };
 
-    while index < 9 {
-        columns.push(&[None; 9]);
-        index += 1;
-    }
-
-    let board = sudoku::board::SudokuBoard::new(columns);
+    let board = sudoku::board::SudokuBoard::new(board_file);
 
     match board {
         Ok(board) => println!("{:}", board),
         Err(message) => panic!("{message}"),
     }
+}
+
+fn read_file(file_path: String) -> Result<Vec<Vec<Option<u8>>>, String> {
+    let mut board_file: Vec<Vec<Option<u8>>> = Vec::new();
+    for line in match read_to_string(file_path) {
+        Ok(file) => file,
+        Err(_) => return Err("Couldn't read the file".to_string()),
+    }
+    .lines()
+    {
+        let list = line
+            .chars()
+            .fold(Vec::<Option<u8>>::new(), |mut acc, value| {
+                acc.push(value.to_digit(10).map(|digit| digit as u8));
+                acc
+            });
+
+        board_file.push(list);
+    }
+
+    Ok(board_file)
 }
--- src/sudoku/board.rs
+++ src/sudoku/board.rs
@@ -1,5 +1,5 @@
-use std::fmt;
 use colored::Colorize;
+use std::fmt;
 
 pub type Quadrant = [[Option<u8>; 3]; 3];
 pub type BoardLine = [Quadrant; 3];
@@ -12,7 +12,9 @@ pub struct SudokuBoard {
 
 impl SudokuBoard {
     const BOARD_DIVIDER: &str = " |";
-    const BOARD_LENGTH: u16 = 9 * 2 + Self::BOARD_DIVIDER.len() as u16 * 3;
+    const BOARD_COLUMN_SIZE: u16 = 9;
+    const BOARD_LENGTH: u16 =
+        (Self::BOARD_COLUMN_SIZE * 2 - 1) + Self::BOARD_DIVIDER.len() as u16 * 2;
 
     fn initialize_quadrant() -> Quadrant {
         ([None, None, None], [None, None, None], [None, None, None]).into()
@@ -39,33 +41,31 @@ impl SudokuBoard {
             .into()
     }
 
-    fn find_quadrant_index_from_column_index(column: usize) -> usize {
-        (f32::trunc((column as f32 / 3.0) * 10.0) / 10.0).floor() as usize
-    }
-
-    pub fn new(list: Vec<&[Option<u8>]>) -> Result<Self, String> {
+    pub fn new(list: Vec<Vec<Option<u8>>>) -> Result<Self, String> {
         if list.len() != 9 {
             return Err("The provided list must have 9 lines".to_string());
         }
 
-        let sudoku_board: SudokuBoard = SudokuBoard {
+        let mut sudoku_board: SudokuBoard = SudokuBoard {
             board: Self::initialize_board(),
         };
 
         for (line_index, row) in list.iter().enumerate() {
-            let board_line_index = (((line_index / 3) as f32).floor()) as usize;
-            let board_line: BoardLine = sudoku_board.board[board_line_index];
-
             if row.len() != 9 {
                 return Err("The provided list must have 9 lines".to_string());
             }
 
-            for (column_index, cell) in row.iter().enumerate() {
-                let quadrant_column_index = ((column_index / 3) as f32).floor() as usize;
-                let quadrant_index = Self::find_quadrant_index_from_column_index(column_index);
-                let mut quadrant = board_line[quadrant_index];
+            let board_line_index = (((line_index / 3) as f32).floor()) as usize;
+            let board_line: &mut BoardLine = &mut sudoku_board.board[board_line_index];
 
-                quadrant[quadrant_index][quadrant_column_index] = *cell;
+            for (column_index, value) in row.iter().enumerate() {
+                let quadrant_index =
+                    (f32::trunc((column_index as f32 / 3.0) * 10.0) / 10.0).floor() as usize;
+                let quadrant_column_index = column_index - quadrant_index * 3;
+                let quadrant_line_index = line_index - board_line_index * 3;
+                let quadrant: &mut Quadrant = &mut board_line[quadrant_line_index];
+
+                quadrant[quadrant_index][quadrant_column_index] = *value;
             }
         }
 
@@ -85,22 +85,26 @@ impl fmt::Display for SudokuBoard {
             let mut column_index: usize = 0;
             let mut line_str = String::new();
 
-            while column_index < 9 {
-                let quadrant_column_index = (column_index as f32 / 3.0).floor() as usize;
-                let quadrant_index: usize =
-                    Self::find_quadrant_index_from_column_index(column_index);
-                let quadrant = board_line[quadrant_index][quadrant_column_index];
-                let quadrant_line_str = quadrant.iter().fold("".to_string(), |acc, value| {
+            while column_index < 3 {
+                let quadrant_index = line_index - board_line_index * 3;
+                let quadrant_line = board_line[quadrant_index][column_index];
+                let quadrant_line_str = quadrant_line.iter().fold("".to_string(), |acc, value| {
                     let value = match value {
                         Some(match_value) => match_value.to_string().blue(),
                         None => "?".to_string().red(),
                     };
-                    format!("{acc} {value}")
+
+                    if acc.len() == 0 {
+                        // Is first column?
+                        format!("{acc}{value}")
+                    } else {
+                        format!("{acc} {value}")
+                    }
                 });
 
                 line_str.push_str(&quadrant_line_str);
                 line_str.push_str(&" |".on_white());
-                column_index += 3;
+                column_index += 1;
             }
 
             if previous_board_line_index != Some(board_line_index) {
