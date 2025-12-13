772fcc0c24986ee21e89ff31d5eb7a39770a1064
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2025-12-13 21:30:33 +1000

    refactor: improve counter

--- src/sudoku/algorithms/backtracking.rs
+++ src/sudoku/algorithms/backtracking.rs
@@ -17,50 +17,53 @@ impl<'a> Backtracking<'a> {
         }
     }
 
+    fn update_and_incr(&mut self, perf: &mut PerfTracker, x: usize, y: usize, value: Option<u8>) -> bool {
+        let res = self.board.update_value(x, y, value);
+        perf.incr();
+        res.is_ok()
+    }
+
     pub fn resolve(self) {
+        let mut this = self;
         let mut backtrack_index = 0usize;
         let mut perf = PerfTracker::new();
 
         perf.start();
 
-        while self.editable_cells.len() > backtrack_index {
-            let (mut x, mut y) = self.editable_cells[backtrack_index];
+        while this.editable_cells.len() > backtrack_index {
+            let (mut x, mut y) = this.editable_cells[backtrack_index];
             let mut current_value = {
-                let cell = self.board.find_cell_from_coordinates(x, y).unwrap();
+                let cell = this.board.find_cell_from_coordinates(x, y).unwrap();
                 cell.value.or(Some(1))
             };
 
             while current_value.unwrap() <= 9 {
                 // thread::sleep(Duration::from_millis(1));
-                perf.incr();
+                if this.update_and_incr(&mut perf, x, y, current_value) {
+                    backtrack_index += 1;
+                    break;
+                } else {
+                    if current_value.unwrap() >= 9 {
+                        let _ = this.board.update_value(x, y, None).unwrap();
+                        perf.incr();
 
-                match self.board.update_value(x, y, current_value) {
-                    Ok(_) => {
-                        backtrack_index += 1;
-                        break;
-                    }
-                    Err(_) => {
-                        if current_value.unwrap() >= 9 {
-                            let _ = self.board.update_value(x, y, None).unwrap();
-                            perf.incr();
-
-                            backtrack_index -= 1;
-                            (x, y) = self.editable_cells[backtrack_index];
-                            let cell = self.board.find_cell_from_coordinates(x, y).unwrap();
-                            current_value = if cell.value.is_some() {
-                                Some(cell.value.unwrap() + 1)
-                            } else {
-                                Some(1)
-                            };
+                        backtrack_index -= 1;
+                        (x, y) = this.editable_cells[backtrack_index];
+                        let cell = this.board.find_cell_from_coordinates(x, y).unwrap();
+                        current_value = if cell.value.is_some() {
+                            Some(cell.value.unwrap() + 1)
                         } else {
-                            current_value = Some(current_value.unwrap() + 1);
-                        }
+                            Some(1)
+                        };
+                    } else {
+                        current_value = Some(current_value.unwrap() + 1);
                     }
                 }
             }
 
             if current_value.unwrap() > 9 {
-                let _ = self.board.update_value(x, y, None).unwrap();
+                let _ = this.board.update_value(x, y, None).unwrap();
+                perf.incr();
                 backtrack_index -= 1;
             }
         }
